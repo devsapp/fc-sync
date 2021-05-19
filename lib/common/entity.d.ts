17b6df0fd5c5626375a3c8ca27ea4fe3422e1a5f
千风 <qianfeng.zqf@alibaba-inc.com>
2021-05-19 14:39:18 +0800

    feat: add sync code ability

---
+++ lib/common/entity.d.ts
@@ -0,0 +1,29 @@
+export interface ICredentials {
+    AccountID?: string;
+    AccessKeyID?: string;
+    AccessKeySecret?: string;
+    SecretID?: string;
+    SecretKey?: string;
+    SecretAccessKey?: string;
+    KeyVaultName?: string;
+    TenantID?: string;
+    ClientID?: string;
+    ClientSecret?: string;
+    PrivateKeyData?: string;
+    stsToken?: string;
+}
+export interface InputProps {
+    props: any;
+    credentials: ICredentials;
+    appName: string;
+    project: {
+        component: string;
+        access: string;
+        projectName: string;
+    };
+    command: string;
+    args: string;
+    path: {
+        configPath: string;
+    };
+}
