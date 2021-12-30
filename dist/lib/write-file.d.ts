805ab813ef2382b9c5c61ed14b0be1a76a3630c9
wss-git <wssgryx@163.com>
2021-12-30 10:37:11 +0800

    build ncc to esbuild & rm doc

--- dist/lib/write-file.d.ts
+++
@@ -1,5 +0,0 @@
-export default class WriteFile {
-    static access: string;
-    static writeSYml(targetDir: any, config: any, fileAffix?: any): Promise<string>;
-    static getYmlFilePath(targetDir: any, fileAffix: any): string;
-}
